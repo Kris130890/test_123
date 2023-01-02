c0967fcf609af4bc3a169bd178e4db03f8d702d9
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2023-01-02 20:38:09 +0100

    comment

--- lib/domain/interactor/country_interactor.dart
+++ lib/domain/interactor/country_interactor.dart
@@ -1,5 +1,5 @@
-import 'package:elinext_test_task/domain/model/country/country.dart';
-import 'package:elinext_test_task/domain/repository/CountryRepository.dart';
+///import 'package:elinext_test_task/domain/model/country/country.dart';
+/*import 'package:elinext_test_task/domain/repository/CountryRepository.dart';
 
 abstract class CountryInteractor {
   Future<Country> loadCountryNews(int countNews);
@@ -16,4 +16,4 @@ class CountryInteractorImpl implements CountryInteractor {
   Future<Country> loadCountryNews(int countNews) async {
     return await _apiRepository.loadCountryNews(countNews);
   }
-}
+}*/
