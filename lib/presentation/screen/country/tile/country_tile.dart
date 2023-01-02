ce0a33b84bf8232c69712dc1bac1803575313a3f
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2023-01-02 20:43:42 +0100

    changed slass

--- lib/presentation/screen/country/tile/country_tile.dart
+++ lib/presentation/screen/country/tile/country_tile.dart
@@ -1,49 +1,28 @@
 import 'package:elinext_test_task/domain/model/country/country.dart';
 
-class CountryTile extends Country {
-  final CountrySourceTile? source;
-  final String? author;
-  final String? title;
-  final String? description;
+class CountryTile extends Pokemon {
+  final String? name;
   final String? url;
-  final String? urlToImage;
-  final String? publishedAt;
-  final String? content;
 
   CountryTile({
-    this.source,
-    this.author,
-    this.title,
-    this.description,
+    this.name,
     this.url,
-    this.urlToImage,
-    this.publishedAt,
-    this.content,
   }) : super(
-          articles: [
-            CountryArticles(
-              source: CountryArticlesSource(
-                id: source?.id,
-                name: source?.name,
-              ),
-              author: author,
-              title: title,
-              description: description,
+    results: [
+      PokemonResults(
+              name: name,
               url: url,
-              urlToImage: urlToImage,
-              publishedAt: publishedAt,
-              content: content,
             ),
           ],
         );
 }
 
-class CountrySourceTile {
-  final String? id;
+class PokemonSourceTile {
+  final String? url;
   final String? name;
 
-  CountrySourceTile({
-    this.id,
+  PokemonSourceTile({
+    this.url,
     this.name,
   });
 }
