41400b3d6776594ecaf1a119099f517e6eaf6685
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2023-01-02 20:40:50 +0100

    add class Pokenom

--- lib/data/api/mapper/country_mapper.dart
+++ lib/data/api/mapper/country_mapper.dart
@@ -2,30 +2,21 @@ import 'package:elinext_test_task/data/api/model/country/api_load_country_respon
 import 'package:elinext_test_task/domain/model/country/country.dart';
 
 class CountryMapper {
-  static Country toCountry(ApiLoadCountryResponse response) {
+  static Pokemon toCountry(PokemonListName response) {
     final data = response;
-    final list = <CountryArticles>[];
-    response?.articles?.forEach((element) {
+    final list = <PokemonResults>[];
+    response.results?.forEach((element) {
       list.add(
-        CountryArticles(
-          source: CountryArticlesSource(
-            id: element.source?.id ?? '',
-            name: element.source?.name ?? '',
-          ),
-          author: element.author,
-          title: element.title,
-          description: element.description,
-          url: element.url,
-          urlToImage: element.urlToImage,
-          publishedAt: element.publishedAt,
-          content: element.content,
+        PokemonResults(
+        name: element.name,
+        url: element.url,
         ),
       );
     });
-    return Country(
-      status: data?.status,
-      totalResults: data?.totalResults,
-      articles: list,
+    return Pokemon(
+      count: data.count,
+      next: data.next,
+      results: list,
     );
   }
 }
