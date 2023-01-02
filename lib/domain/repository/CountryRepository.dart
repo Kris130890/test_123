88d7681d97e115dc03c39e34092ac45a7a412cae
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2023-01-02 20:37:11 +0100

    comment

--- lib/domain/repository/CountryRepository.dart
+++ lib/domain/repository/CountryRepository.dart
@@ -1,5 +1,5 @@
-import 'package:elinext_test_task/domain/model/country/country.dart';
+/*import 'package:elinext_test_task/domain/model/country/country.dart';
 
 abstract class CountryRepository {
   Future<Country> loadCountryNews(int countNews);
-}
+}*/
