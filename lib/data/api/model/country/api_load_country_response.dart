245f09ef9202497952e3b304d2aa4757d4afa39b
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2023-01-02 12:39:54 +0100

    add new class

--- lib/data/api/model/country/api_load_country_response.dart
+++ lib/data/api/model/country/api_load_country_response.dart
@@ -1,98 +1,35 @@
-class ApiLoadCountryResponse {
-  final status;
-  final totalResults;
-  final List<ApiLoadCountryArticlesResponse>? articles;
+class PokemonListName {
+  final count;
+  final next;
+  Null previous;
+  List<PokemonsResultsList>? results;
 
-  ApiLoadCountryResponse(
-    this.status,
-    this.totalResults,
-    this.articles,
-    Map<String, dynamic> json,
-  );
+  PokemonListName(this.count, this.next, this.previous, this.results, Map<String, dynamic> json,);
 
-  static ApiLoadCountryResponse fromJson(Map<String, dynamic> json) {
-    final status = json['status'];
-    final totalResults = json['totalResults'];
-    final articles = json['articles'];
-    return ApiLoadCountryResponse(
-      status,
-      totalResults,
-      articles == null
+  PokemonListName? fromJson(Map<String, dynamic> json) {
+    final count = json['count'];
+    final next = json['next'];
+    final previous = json['previous'];
+    return PokemonListName(
+      count,
+      next,
+      previous,
+      results == null
           ? null
-          : ApiLoadCountryArticlesResponse.fromJson(articles),
+          : PokemonsResultsList.fromJson(results),
       json,
     );
   }
 }
 
-class ApiLoadCountryArticlesResponse {
-  final ApiLoadCountryArticlesSourceResponse? source;
-  final String? author;
-  final String? title;
-  final String? description;
-  final String? url;
-  final String? urlToImage;
-  final String? publishedAt;
-  final String? content;
+class PokemonsResultsList {
+  final name;
+  final url;
 
-  ApiLoadCountryArticlesResponse(
-    this.source,
-    this.author,
-    this.title,
-    this.description,
-    this.url,
-    this.urlToImage,
-    this.publishedAt,
-    this.content,
-    List<dynamic> json,
-  );
+  PokemonsResultsList({this.name, this.url});
 
-  static List<ApiLoadCountryArticlesResponse> fromJson(List<dynamic> json) {
-    final list = <ApiLoadCountryArticlesResponse>[];
-
-    json.forEach((element) {
-      final source = element['source'];
-      final author = element['author'];
-      final title = element['title'];
-      final description = element['description'];
-      final url = element['url'];
-      final urlToImage = element['urlToImage'];
-      final publishedAt = element['publishedAt'];
-      final content = element['content'];
-      list.add(
-        ApiLoadCountryArticlesResponse(
-          source == null
-              ? null
-              : ApiLoadCountryArticlesSourceResponse.fromJson(source),
-          author,
-          title,
-          description,
-          url,
-          urlToImage,
-          publishedAt,
-          content,
-          json,
-        ),
-      );
-    });
-
-    return list;
+  PokemonsResultsList? fromJson(Map<String, dynamic> json) {
+    final name = json['name'];
+    final url = json['url'];
   }
-}
-
-class ApiLoadCountryArticlesSourceResponse {
-  final String? id;
-  final String? name;
-
-  ApiLoadCountryArticlesSourceResponse(
-    this.id,
-    this.name,
-  );
-
-  static ApiLoadCountryArticlesSourceResponse fromJson(
-          Map<String, dynamic> json) =>
-      ApiLoadCountryArticlesSourceResponse(
-        json['id'],
-        json['name'],
-      );
 }
